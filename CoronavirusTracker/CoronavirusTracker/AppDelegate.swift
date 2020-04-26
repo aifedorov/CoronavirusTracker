14fedb40b19e26dbe1127da32e66211055b8e280
Aleksandr <fairk19@gmail.com>
2020-04-26 17:21:33 +0300

    Fix display stats and add timer

--- CoronavirusTracker/CoronavirusTracker/AppDelegate.swift
+++ CoronavirusTracker/CoronavirusTracker/AppDelegate.swift
@@ -12,12 +12,44 @@ import SwiftUI
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    var statusBarItem: NSStatusItem!
-
     private var webservice = WebService()
+    private var statusBarItem: NSStatusItem!
+    private var timer: Timer!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         statusBarItem = makeStatusBarItem()
+        setupTimer()
+    }
+
+    // MARK: Private
+
+    private func makeStatusBarItem() -> NSStatusItem {
+        let statusBar = NSStatusBar.system
+        let statusBarItem = statusBar.statusItem(withLength: NSStatusItem.variableLength)
+        let menu = NSMenu()
+        let title = "quit".localized
+        menu.addItem(NSMenuItem(title: title, action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+
+        if let button = statusBarItem.button {
+            button.title = "🦠⏳"
+        }
+
+        statusBarItem.menu = menu
+        return statusBarItem
+    }
+
+    private func requestStats(completion: @escaping (CountryStats?) -> Void) {
+        let url = URL(string: "https://corona.lmao.ninja/v2/countries/ru")!
+        let resource = Resource<CountryStats>(url: url) { (data) -> CountryStats? in
+            return try? JSONDecoder().decode(CountryStats.self, from: data)
+        }
+
+        webservice.load(resource) { stats in
+            completion(stats)
+        }
+    }
+
+    private func refreshStats() {
         requestStats { [unowned self] (stats) in
             let title: String
             if let stats = stats {
@@ -33,27 +65,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
     }
 
-    // MARK: Private
-
-    private func makeStatusBarItem() -> NSStatusItem {
-        let statusBar = NSStatusBar.system
-        let statusBarItem = statusBar.statusItem(withLength: NSStatusItem.variableLength)
-        let menu = NSMenu()
-        let title = "quit".localized
-        menu.addItem(NSMenuItem(title: title, action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
-
-        statusBarItem.menu = menu
-        return statusBarItem
-    }
-
-    private func requestStats(completion: @escaping (CountryStats?) -> Void) {
-        let url = URL(string: "https://corona.lmao.ninja/v2/countries/ru")!
-        let resource = Resource<CountryStats>(url: url) { (data) -> CountryStats? in
-            return try? JSONDecoder().decode(CountryStats.self, from: data)
+    func setupTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 60 * 60 * 12, repeats: true) { [weak self] _ in
+            guard let self = self else { return }
+            self.refreshStats()
         }
 
-        webservice.load(resource) { stats in
-            completion(stats)
-        }
+        timer.fire()
     }
 }
